f2e0ad3bb89feb9668ad07573908ca92d16031c3
Rachel Dodell <rachel@racheldodell.com>
2018-06-22 21:35:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidayto(holiday, name) {
+ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
 }
